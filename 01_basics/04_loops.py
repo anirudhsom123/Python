77028913ddf5a250ac17e6866266d1f37d0554ee
aniurdhsom123 <anirudhsom007@gmail.com>
2026-02-14 23:37:23 +0530

    loops

---
+++ 01_basics/04_loops.py
@@ -0,0 +1,22 @@
+num=int(input("Enter the number : "))
+
+# while loop
+i=1
+
+while(i<=10):
+    print(i*num)
+    i+=1
+
+# while with else
+
+k=0
+while k<3:
+    print(k)
+    k+=1
+else:
+    print("while loop ended")
+
+    
+# for loop 
+for j in range(1,11):
+    print(j*num)
