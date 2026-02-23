f88dce9323be54b22dde2711d931f306536b146c
aniurdhsom123 <anirudhsom007@gmail.com>
2026-02-23 23:55:22 +0530

    annotated and Field

--- 06_pydantic_validation/03_pydantic.py
+++ 06_pydantic_validation/03_pydantic.py
@@ -1,11 +1,13 @@
 # data validation offered by pydantic
-from pydantic import EmailStr , BaseModel , AnyUrl
-from typing import Optional
+from pydantic import EmailStr , BaseModel , AnyUrl , Field
+from typing import Optional , Annotated
 
 class Patient(BaseModel):
-    name: str
+    name: Annotated[str,Field(max_length=40,title="name of patient",description="enter patient name only 40 characters are allowed ",examples=['Ani','kapoor'])]
     email : Optional[EmailStr] = None
     Linkedin_url : Optional[AnyUrl] = None
+    married : Annotated[bool,Field(default=False ,description="is marreid or not")]
+    age : int = Field(gt=10,le=100)
   
 # add Patient to DB 
 def add_patient(patient : Patient):
@@ -14,7 +16,9 @@ def add_patient(patient : Patient):
     
 info={
     'name' : 'anirudh' ,
-    'email' : 'anirudh@gmail.com'
+    'email' : 'anirudh@gmail.com',
+   #  'Linkedin_url' :'abc' 
+   'age' : 11
 } 
 
 patient1=Patient(**info)
